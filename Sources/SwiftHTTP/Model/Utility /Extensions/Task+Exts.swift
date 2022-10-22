37a583329728ed7725578e48bec0773038173a64
dylanjrafferty <34669101+dylanjrafferty@users.noreply.github.com>
2022-10-22 12:09:40 -0400

    Update Task+Exts.swift

--- Sources/SwiftHTTP/Model/Utility /Extensions/Task+Exts.swift
+++ Sources/SwiftHTTP/Model/Utility /Extensions/Task+Exts.swift
@@ -11,18 +11,16 @@ extension Task where Failure == Error {
     @discardableResult
     static func retrying(
         priority: TaskPriority? = nil,
-        maxRetryCount: Int = 0,
-        retryDelay: TimeInterval = 0,
+        maxRetryCount: Int = 3,
         operation: @Sendable @escaping () async throws -> Success
     ) -> Task {
         Task(priority: priority) {
             for _ in 0..<maxRetryCount {
+                try Task<Never, Never>.checkCancellation()
+
                 do {
                     return try await operation()
                 } catch {
-                    let oneSecond = TimeInterval(1_000_000_000)
-                    let delay = UInt64(oneSecond * retryDelay)
-                    try await Task<Never, Never>.sleep(nanoseconds: delay)
                     continue
                 }
             }
